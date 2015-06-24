e9969d4ae215b175e113ef77e66f3e13530999a9
Олег <oleg@MBP.local>
2015-06-25 00:41:44 +0300

    Prortrolio images ready, but not optimized

--- gulpfile.js
+++ gulpfile.js
@@ -37,10 +37,10 @@ gulp.task('jshint', function () {
  
 gulp.task('images', function () {
   return gulp.src('app/images/**/*')
-    .pipe($.cache($.imagemin({
-      progressive: true,
-      interlaced: true
-    })))
+    // .pipe($.cache($.imagemin({
+    //   progressive: true,
+    //   interlaced: true
+    // })))
     .pipe(gulp.dest('dist/images'));
 });
  
